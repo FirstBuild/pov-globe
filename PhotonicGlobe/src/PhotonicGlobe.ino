76dc58e3f2a9e1882a955b59cf542319e5dde758
James Kim <james@firstbuild.com>
2017-10-16 18:32:40 -0400

    feat: Implement basic interrupt based rotation counter using hall effect sensor

--- PhotonicGlobe/src/PhotonicGlobe.ino
+++ PhotonicGlobe/src/PhotonicGlobe.ino
@@ -5,14 +5,29 @@
  * Date:
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+#include "RBD_Timer.h"
 
+volatile int rpmCount = 0;
+RBD::Timer printOutput;
+
+void rpmCounter() {
+  rpmCount++;
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void setup() {
+  Serial.begin(9600);
+  printOutput.setTimeout(3000);
 
+  pinMode(6, INPUT_PULLUP);
+  attachInterrupt(6, rpmCounter, FALLING);
+
+  printOutput.restart();
+}
+
+void loop() {
+  if (printOutput.onExpired()) {
+    Serial.print("RPM: ");
+    Serial.println(rpmCount);
+    printOutput.restart();
+  }
 }
