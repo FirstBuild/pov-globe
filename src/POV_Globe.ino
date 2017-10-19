d6daa95f3f036cb2c10825edd12ecc79c69c7e92
James Kim <james@firstbuild.com>
2017-10-19 17:51:23 -0400

    refactor: Deleting dead code and moving towards basic text display

--- src/POV_Globe.ino
+++ src/POV_Globe.ino
@@ -10,8 +10,8 @@
 #include "bitArrayImages.h"
 
 #define hallEffectSensorPin 6
-#define CLK 4
-#define DATA 2
+#define CLK A5
+#define USE_HW_SPI A3
 
 #define NUM_LEDS 260
 #define BRIGHTNESS 150
@@ -23,115 +23,85 @@
 #define SPARKING 150
 #define COOLING  100
 
-volatile int rotationCounter = 0;
-volatile bool update = false;
-int rotationsPerSec = 0;
-int rotationsPerMin = 0;
-long timePerSlice = 0;
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
 
 RBD::Timer printOutput;
-RBD::Timer rapidUpdate;
+Adafruit_DotStar strip = Adafruit_DotStar(NUM_LEDS);
 
-Adafruit_DotStar strip = Adafruit_DotStar(NUM_LEDS, DATA , CLK);
-
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(SEMI_AUTOMATIC);
-
-int      head  = 0, tail = -260; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0022;      // 'On' color (starts orange)
 
-void rpmCounter() {
-  update=true;
-}
+void rpmCounter();
+void draw_column();
+void modifyTime();
+
+unsigned long startTimeOfRotation = micros();
+unsigned long timeLengthOfRotation = micros() - startTimeOfRotation;
+unsigned long timePerColumn = timeLengthOfRotation / 360;
+volatile bool update = false;
+int cur_col = 0;
+int pos = 0;
 
 void setup() {
-  printOutput.setTimeout(1000);
-
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
 
   pinMode(hallEffectSensorPin, INPUT_PULLUP);
   attachInterrupt(hallEffectSensorPin, rpmCounter, FALLING);
-  printOutput.restart();
+}
+
+void rpmCounter() {
+  update = true;
 }
 
 void loop() {
-  //hallEffectHandler();
-  //static unsigned long test = micros();
-  //static int count = 0;
+  //while (cur_col != pos) {
+    draw_column();
+    
+    static unsigned long tracker = micros();
+    if ((micros() - tracker) > timePerColumn) {
+      if (++cur_col == 360) {
+        cur_col = 0;
+      }
+      tracker = micros();
+    } 
 
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
+  //while(startTimeOfRotation < timePerColumn) {} 
 
-  if(++head >= NUM_LEDS) {         // Increment head index.  Off end of strip?
-    head = 0;                       //  Yes, reset head index to start
-    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-      color = 0x00A5FF;             //   Yes, reset to red
-  }
-  if(++tail >= NUM_LEDS) tail = 0; // Increment, reset tail index
-
-  //if (printOutput.onExpired()) {
-  //    timePerSlice = ((micros() - test) * 1000) / 360;
-  //    test = micros();
-  //    printOutput.setTimeout(timePerSlice);
-  //    printOutput.restart();
+  //modifyTime();
+  //while (startTimeOfRotation >= timePerColumn) {
+  //  modifyTime();
   //}
-  //showLights(count);
-  //count++;
-//
-  //if (update) {
-  //  update = false;
-  //  count = 0;
+
+  //if (pos == 0) {
+    if (update) {
+      update = false;
+      timeLengthOfRotation = micros() - startTimeOfRotation;
+      timePerColumn = timeLengthOfRotation / 360;
+      startTimeOfRotation = micros();
+      cur_col = 0;
+      //pos = 0;
+    }
   //}
 }
 
-
-void showLights(int index) {
-
-  if (index == 0) {
-    for (int i = 0; i < NUM_LEDS; i++) {
+void modifyTime() {
+    startTimeOfRotation -= timePerColumn;
+    if (++pos == 360) {
+      pos = 0;
     }
-  } else {
-  }
 }
 
-
-void roundTest() {
-  if (printOutput.onExpired()) {
-    static unsigned int tracker = 0;
-    pattern(tracker);
-    tracker++;
-    if (tracker == NUM_LEDS) {
-      tracker = 0;
+void draw_column() {
+  if (cur_col > 54) {
+    strip.clear();
+  }
+  for (int i = 0; i < 18; i++) {
+    if (largeHelloText[i][cur_col]) {
+      strip.setPixelColor(i + 55, color);
+    } else {
+      strip.setPixelColor(i + 55, 0);
     }
-    printOutput.restart();
   }
-}
-
-void pattern(int test) {
-}
-
-void hallEffectHandler() {
-  if (printOutput.onExpired()) {
-    noInterrupts();
-    rotationsPerMin = rotationCounter * 60;
-    rotationsPerSec = rotationCounter;
-
-    timePerSlice = (int) floor((1.0 / rotationsPerSec) * 1000) ;
-    // Rotations per minute
-    Serial.print("RPM: ");
-    Serial.println(rotationsPerMin);
-
-    // Rotations per second
-    Serial.print("RPS: ");
-    Serial.println(rotationsPerSec);
-
-    rotationCounter = 0;
-
-
-    printOutput.restart();
-    interrupts();
-  }
+  strip.show();
 }
